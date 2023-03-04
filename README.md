c9bbb4cd699fc5fa20a94211105c519259195a39
jdeath <17914369+jdeath@users.noreply.github.com>
2023-03-03 23:15:58 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# fdmealplanner for Homeassistant
+
+Place fdmealplanner/ directory inside /config/custom_components/
+
+In your /config/configuration.yaml add a block under sensors:
+
+```
+- platform: fdmealplanner
+  accounts:
+    - 'XX/YY/Z'
+
+```      
+      
+`/XX/YY/Z` is the codes in the FD MealPanner URL: https://www.fdmealplanner.com/ for your school and meal type. For instance if you choose "Huntington Washington"  and the lunch meal, the URL you will be taken to is: `https://www.fdmealplanner.com/#menu/mealPlanner/27/65/2` so use `27/65/2` for your code.
+
+This will create a sensor called sensor.fdmealplanner_27_65_2 with attributes of lunch0, lunch1, lunch2, lunch3, lunch4 which are the lunches of today - five days from now.
+Multiple schools should work, but not tested.
+
+This code works for me, but may need to be changed if your school is more complicated. My school has lots of items on the menu, so line 119 in sensor.py truncates to 7 items. Change it to whatever you want.
+
+I am using a markdown card to display the first 3 entries.
+
+```
+type: markdown
+content: |-
+  {{ states.sensor.fdmealplanner_27_65_2.attributes.lunch0 }}
+  {{ states.sensor.fdmealplanner_27_65_2.attributes.lunch1 }}
+  {{ states.sensor.fdmealplanner_27_65_2.attributes.lunch2 }}
+title: School Lunch
+
+```
+Let me know if have any suggestions. Right now I leave the entry blank if no information. Each school gets updated once every 6 hours. 
---
+++ custom_components/mealviewer/sensor.py
@@ -0,0 +1,164 @@
+"""Sensor for fdmealplanner account status."""
+from datetime import timedelta
+import logging
+import requests
+import arrow
+import xmltodict, json
+from xml.etree import ElementTree
+from itertools import islice
+import urllib.parse
+
+from time import mktime
+
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.event import track_time_interval
+from homeassistant.util.dt import utc_from_timestamp
+
+_LOGGER = logging.getLogger(__name__)
+
+CONF_ACCOUNTS = "accounts"
+
+ICON = "mdi:robot-outline"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_ACCOUNTS, default=[]): vol.All(cv.ensure_list, [cv.string]),
+    }
+)
+
+
+BASE_INTERVAL = timedelta(hours=6)
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the fdmealplanner platform."""
+    
+    entities = [fdmealplannerSensor(account) for account in config.get(CONF_ACCOUNTS)]
+    if not entities:
+        return
+    add_entities(entities, True)
+
+    # Only one sensor update once every 60 seconds to avoid
+    entity_next = 0
+
+
+
+class fdmealplannerSensor(Entity):
+    """A class for the mealviewer account."""
+
+    def __init__(self, account):
+        """Initialize the sensor."""
+        
+        self._account = account
+        self._lunch0 = None
+        self._lunch1 = None
+        self._lunch2 = None
+        self._lunch3 = None
+        self._lunch4 = None
+        self._state = None
+        self._name = self._account
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def entity_id(self):
+        """Return the entity ID."""
+        #return f"sensor.mealviewer_{self._name}"
+        return 'sensor.fdmealplanner_' + (self._account).replace("/","_")
+        
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+    
+    @property
+    def should_poll(self):
+        """Turn off polling, will do ourselves."""
+        return True
+        
+    def update(self):
+        """Update device state."""
+        
+        self._state = 'Not Updated'
+        try:
+           
+            headers = {
+                'accept': 'application/json',
+                'pragma': 'no-cache',
+                'x-jsonresponsecase': 'camel',
+                'x-requested-with': 'XMLHttpRequest',
+                'user-agent': 'okhttp/4.9.1',
+            }
+
+            [accountId, locationId, mealPeriodId] = self._account.split('/')
+            for day in range(5):
+                tomorrow = arrow.utcnow().to('US/Eastern').shift(days=day)
+                meal = ''
+                if tomorrow.weekday() not in (5, 6):
+                    formatted_tomorrow = urllib.parse.quote(tomorrow.format('MM DD YYYY'))
+                    formatted_year = tomorrow.format('YYYY')
+                    formatted_month = tomorrow.format('M')   
+                    
+                    url = 'https://apiservicelocators.fdmealplanner.com/api/v1/data-locator-webapi/3/meals?accountId=' + accountId + '&endDate=' + formatted_tomorrow + '&isActive=true&isStandalone&locationId='+ locationId +'&mealPeriodId=' + mealPeriodId + '&menuId=0&monthId=' +formatted_month+ '&selectedDate=' + formatted_tomorrow + '&startDate=' + formatted_tomorrow + '&tenantId=3&timeOffset=300&year=' + formatted_year
+                    
+                    response = requests.get(url,headers=headers,)
+                    if response.json()['result'][0].get('xmlMenuRecipes') is None:
+                        meal = ''
+                    else:                   
+                        root = ElementTree.fromstring(response.json()['result'][0].get('xmlMenuRecipes'))
+                        meal = tomorrow.format('dddd')
+                        counter = 0
+                        for child in islice(root,7):
+                            counter = counter + 1
+                            entree = child.attrib.get('ComponentEnglishName').strip()
+                            if counter == 1:
+                                meal = meal + ': ' + entree
+                            else:
+                                meal = meal + ', ' + entree
+                
+                if day == 0:
+                    self._lunch0 = meal
+                if day == 1:
+                    self._lunch1 = meal
+                if day == 2:
+                    self._lunch2 = meal
+                if day == 3:
+                    self._lunch3 = meal
+                if day == 4:
+                    self._lunch4 = meal
+                    
+                self._state = 'Updated'    
+        except:        
+            self._lunch0 = None
+            self._lunch1 = None
+            self._lunch2 = None
+            self._lunch3 = None
+            self._lunch4 = None
+    ff
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attr = {}
+        
+        attr["lunch0"] = self._lunch0
+        attr["lunch1"] = self._lunch1    
+        attr["lunch2"] = self._lunch2
+        attr["lunch3"] = self._lunch3
+        attr["lunch4"] = self._lunch4
+        
+ 
+        return attr
+
+    
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend."""
+        return ICON
